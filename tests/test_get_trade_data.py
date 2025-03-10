8de72828d47bcd9b047cfa09d671e9cbcab4613c
Jose Pajuelo <jpajuelo@uchicago.edu>
2025-03-10 05:35:04 -0500

    Simmulation page

--- tests/test_get_trade_data.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from src.get_trade_data import (combine_url_with_params, 
-                             url_to_cache_key, get_data_census)
-
-def test_url_to_cache_key():
-    url = "https://api.census.gov/data/timeseries/intltrade/imports/statenaics?get=CTY_CODE"
-    result = url_to_cache_key(url)
-    assert result == "api.census.gov_data_timeseries_intltrade_imports_statenaics_get=cty_code"
